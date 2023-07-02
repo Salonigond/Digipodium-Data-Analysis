627c02c0972f7f4cde13e2bd37d598f690b682c4
salonigond <salonigond01@gmail.com>
2023-07-02 11:52:44 +0530

    task4

---
+++ i.py
@@ -0,0 +1,13 @@
+from turtle import*
+
+speed('slowest')
+fd(50)
+bk(25)
+lt(90)
+fd(160)
+rt(90)
+fd(25)
+bk(50)
+
+hideturtle()
+mainloop()
---
+++ t6.py
@@ -0,0 +1,14 @@
+from turtle import*
+
+speed('slowest')
+rt(60)
+fd(160)
+bk(160)
+rt(60)
+fd(160)
+bk(80)
+lt(120)
+fd(80)
+
+hideturtle()
+mainloop()
